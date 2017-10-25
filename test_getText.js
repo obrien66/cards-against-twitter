dab5de2097a4eb9350b79649b4322adacfe2924c
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-10-25 12:45:38 -0400

    debugged

--- test_getText.js
+++ test_getText.js
@@ -1,18 +1,8 @@
 const {blackCards, whiteCards} = require("./decks")
-var Twit = require("twit")
-var conf = require("./config")
-
-var T = new Twit({
-	consumer_key: conf.consumerKey,
-	consumer_secret: conf.consumerSecret,
-	access_token: conf.accessToken,
-	access_token_secret: conf.accessSecret
-})
 
 const blackLength = blackCards.length
 const whiteLength = whiteCards.length
 const regex = new RegExp("_")
-
 
 function getText(){
 	var blackCardPos = Math.floor(Math.random() * (blackLength))
@@ -43,21 +33,3 @@ function getText(){
 
 	return finish
 }
-
-
-function post(text){
-	T.post("statuses/update", {status: text}, (err, data, res) => {
-		if (err) {
-			throw err
-		}
-	})
-}
-
-post(getText())
-
-let tweetnum = 0
-setInterval(function(){
-	post(getText())
-	tweetnum++
-	console.log(tweetnum)
-}, conf.timeInMinutes*60*1000)
